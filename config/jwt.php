ed18852a2afb4e6ce910c9591422e72687cabba4
liux <lg970044@foxmail.com>
2023-12-18 17:26:09 +0800

    fix: jwt读取

--- config/jwt.php
+++ config/jwt.php
@@ -2,15 +2,15 @@
 
 return [
     // 加密算法
-    'algo'        => env('JWT_ALGO', 'HS256'),
-    'secret'      => env('JWT_SECRET'),
+    'algo'        => env('jwt.algo', 'HS256'),
+    'secret'      => env('jwt.secret'),
     // Time To Live(单位秒，默认半小时)
-    'ttl'         => env('JWT_TTL', 3600),
+    'ttl'         => env('jwt.ttl', 1800),
     // Refresh Time To Live(单位分，默认14天)
-    'refresh_ttl' => env('JWT_REFRESH_TTL', 20160),
+    'refresh_ttl' => env('jwt.refresh_ttl', 20160),
     // Token获取方式，靠前优先
     'token_mode'    => ['header', 'param'],
     // 黑名单宽限期(秒)
-    'blacklist_grace_period' => env('BLACKLIST_GRACE_PERIOD', 10),
+    'blacklist_grace_period' => env('jwt.blacklist_grace_period', 10),
     'blacklist_storage' => thans\jwt\provider\storage\Tp6::class,
 ];
